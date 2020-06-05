f42d0ca6b66705f67ca631fb7aec4f7aeab58267
Guestapo-rlst <kamiljonruziyev@mail.ru>
2020-06-06 02:10:45 +0500

    js_0

---
+++ js/script.js
@@ -0,0 +1,151 @@
+'use strict';
+
+/*var leftBorderWidth = 1;
+{
+    let second = 2;
+}
+const pi = 3.14; */
+
+/*var number = 5;
+var string = "Hello!";
+var symbol = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+console.log(4/0);
+console.log("string"*9);
+
+let something;
+console.log(something);
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(person.name);
+console.log(person["name"]);
+
+let array = ["plum.png", "orange.jpeg", "apple.bmp"];
+
+console.log(array[0]); */
+
+/* //  - to String
+
+// 1)
+String(null);
+console.log(typeof(String(null)));
+console.log(typeof(String(true)));
+console.log(typeof(String(675)));
+
+// 2)
+console.log("Do" + "da");
+console.log("Doda" + "42");
+console.log("Doda" + false);
+console.log(typeof("Doda" + "42"));
+console.log('https://vk.com/catalog/' + 5);
+
+//  - to Number 
+
+// 1)
+console.log(typeof(Number(null)));
+console.log(typeof(Number("Dooda")));
+
+// 2)
+console.log(typeof(+'5'));
+console.log(typeof(35 + +'5'));
+console.log(35 + +"5");
+
+// 3)
+console.log(typeof(parseInt('15px', 10)));
+
+let answer = +prompt("Hello?", '');
+
+//  - to Boolean
+
+// 1)
+
+// always false(nulls):
+0, '', null, undefined, NaN
+
+// notnulls:
+' '
+
+let switcher = null;
+
+if (switcher) {
+    console.log("Working...");
+}
+
+switcher = 1;
+
+if(switcher) {
+    console.log("Working...");
+}
+
+// 2)
+console.log(typeof(Boolean('5')));
+
+// 3)
+console.log(typeof(!!56)); */
+
+/* // alert("Hello, World!");
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+let answer = prompt("Are you over 18 years old?", "");
+
+console.log(answer);
+
+let answer = prompt("Are you over 18 years old?", "Yes");
+
+console.log(typeof(answer));
+
+console.log(typeof(null)); */
+
+/* console.log("Guestapo is a " + "human");
+console.log(1 + "-human");
+
+let answer = +prompt("Are you over 18 years old?", "Yes");
+
+console.log(typeof(answer));
+
+console.log(1 + +"-human");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+let incr1 = 10,
+    decr1 = 10;
+
+console.log(++incr1);
+console.log(--decr1);
+
+let incr2 = 10,
+    decr2 = 10;
+
+console.log(incr2++);
+console.log(decr2--);
+
+console.log(5%2);
+console.log("2" == 2); //comparison by values
+console.log("2" === 2); // comparison by data types (i.e. Number, String, Boolean, etc.)
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose); //true if both (all) objects are true
+console.log(isChecked || isClose); //true if one of the objects is true
+
+console.log(isChecked && !isClose); // ! turns boolean type of the object to the opposite (i.e. false to true, and vice versa) */
