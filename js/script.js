04771eb6b71b007751c0825d01b731745478ba24
Guestapo-rlst <kamiljonruziyev@mail.ru>
2020-06-21 22:53:47 +0500

    till functions

--- js/script.js
+++ js/script.js
@@ -190,6 +190,7 @@ switch (num) {
 
 // First task
 
+/*
 let money = prompt("How much is your monthly budget?", "")
 let time = prompt("Input the date in YYYY-MM-DD format.", "")
 
@@ -211,3 +212,185 @@ appData.expenses[exp_q1_1] = exp_q1_2;
 appData.expenses[exp_q2_1] = exp_q2_2;
 
 alert(appData.budget / 30);
+*/
+
+// let whl = 500;
+// while (whl < 510) {
+//     console.log(whl);
+//     whl++;
+// }
+
+// let dwhl = 500
+// do {
+//     console.log(dwhl);
+//     dwhl++;
+// }
+// while (dwhl < 509)
+
+// for (let frlp = 1; frlp < 9; frlp++) {
+//     if (frlp == 3) {
+//         continue;
+//     }
+//     if (frlp == 5) {
+//         break;
+//     }
+//     console.log(frlp);
+// }
+
+// Second task
+/*
+// let x = 5;
+// alert(x++);
+
+// console.log([] + false - null + true);
+
+// let y = 1;
+// let x = y = 2;
+// alert(x);
+
+// console.log([] + 1 + 2);
+
+// alert("1"[0]);
+
+// console.log(2 && 1 && null && 0 && undefined);
+
+// let a = 1,
+//     b = 89;
+// console.log(!!(a && b));
+// console.log(a && b);
+
+// alert(null || 2 && 3 || 4);
+
+// let a = [1, 2, 3];
+// let b = [1, 2, 3];
+// if (a == b) {
+//     console.log("True");
+// } else {
+//     console.log("False");
+// }
+
+// alert(+"Infinity");
+
+// console.log(!!("ejik" > "yabloko"));
+
+// console.log(0 || "" || 2 || undefined || true || false);
+*/
+
+// Third task
+/*
+let money = +prompt("How much is your monthly budget?", "")
+let time = prompt("Input the date in YYYY-MM-DD format.", "")
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false
+};
+
+// for( let i = 0; i < 2; i++) {
+//     let a = prompt("Enter the required expenditure item for this month.", ""),
+//         b = prompt("How much will it cost?", "");
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("Done!");
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log("Wrong input");
+//         i--;
+//     }
+// }
+
+// let i = 0
+// while (i < 2) {
+//     let a = prompt("Enter the required expenditure item for this month.", ""),
+//         b = prompt("How much will it cost?", "");
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("Done!");
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log("Wrong input");
+//         i--;
+//     }
+//     i++;
+// }
+
+// let i = 0;
+// do {
+//     let a = prompt("Enter the required expenditure item for this month.", ""),
+//         b = prompt("How much will it cost?", "");
+//     if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+//         console.log("Done!");
+//         appData.expenses[a] = b;
+//     } else {
+//         console.log("Wrong input");
+//         i--;
+//     }
+//     i++;
+// } while (i < 2)
+
+appData.moneyDaily = appData.budget / 30;
+
+alert("Daily budget is " + appData.moneyDaily);
+
+if (appData.moneyDaily <= 100) {
+    console.log("Minimum income level");
+} else if (appData.moneyDaily > 100 && appData.moneyDaily < 2000) {
+    console.log("Average income level");
+} else if (appData.moneyDaily > 2000) {
+    console.log("High income level");
+} else {
+    console.log("Error");
+}
+*/
+
+// let num = 20;
+// function showFirstMessage (text) {
+//     alert(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage("Hello, world!");
+// console.log(num);
+
+// console.log(calc(8,4));
+
+// function calc(a,b) {
+//     return (a + b);
+// } // можно вызвать и до, и после объявления функции (function declaration)
+
+// console.log(calc(3,4));
+
+
+// function retVar() {
+//     let num = 50;
+//     return num;
+// }
+
+// let anotherNum = retVar();
+// console.log(anotherNum);
+
+// let calcNew = function(a,b) {
+//     return (a + b);
+// } // невозможно вызвать функцию перед её объявлением (function expression)
+
+// let calcNeww = (a,b) => a + b
+
+// console.log(calcNeww(3,4));
+
+let str = "test";
+console.log(str.length);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let twelve = "12.7";
+
+console.log(Math.round(twelve));
+
+let uno = "12.7px";
+
+console.log(parseInt(uno));
+console.log(parseFloat(uno));
